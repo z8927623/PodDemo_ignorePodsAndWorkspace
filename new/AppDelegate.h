031996c4d12cf27e47bb5eacc0a629eaa77b7712
Wildyao <z8927623@163.com>
2015-01-14 16:51:25 +0800

    First commit

---
+++ new/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  new
+//
+//  Created by wildyao on 14/12/25.
+//  Copyright (c) 2014年 Wild Yaoyao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
